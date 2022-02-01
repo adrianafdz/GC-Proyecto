66ae61b930eb70f483584e0eb276f2870a9a1033
adrianafdz <adrianafdz9@gmail.com>
2022-01-31 20:05:50 -0600

    Importar silla

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import * as THREE from "./three.module.js";
 import { OrbitControls } from "./OrbitControls.js";
+import { OBJLoader } from "./OBJLoader.js";
+import { MTLLoader } from "./MTLLoader.js";
 import { crearTorre } from "./Objetos/torre.js";
 import { crearCuarto } from "./Objetos/room.js";
 import { crearDesk } from "./Objetos/desk.js";
@@ -23,8 +25,7 @@ const miny = 5;
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
-    5,
-    150
+    5
 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -108,6 +109,26 @@ sillon.rotation.y = Math.PI;
 sillon.position.z = 13;
 sillon.position.x = 10;
 scene.add(sillon);
+
+var ambientLight = new THREE.AmbientLight('#fff');
+scene.add(ambientLight);
+
+// SILLA IMPORTADA
+// // https://free3d.com/3d-model/office-chair-871087.html
+const sillaMtl = new MTLLoader().load('./Objetos/Office_chair.mtl', function(materials) {
+    materials.preload();
+    
+    new OBJLoader().setMaterials(materials).load(
+        './Objetos/Office_chair.obj', 
+        (object) => {
+            object.rotation.y = Math.PI;
+            object.scale.x = 0.013;
+            object.scale.y = 0.013;
+            object.scale.z = 0.013;
+            object.position.z = -6;
+            scene.add( object );
+        });
+})
 
 // animaciones
 let animate = () => {
